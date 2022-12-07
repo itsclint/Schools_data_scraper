3463b1043b05b80ed1053c53e98ff0295ced1334
clinton <mbatakuclinton@gmail.com>
2022-12-07 17:47:09 +0100

    Data processing and download button added

--- streamlit_app/streamlit_app.py
+++ streamlit_app/streamlit_app.py
@@ -9,6 +9,7 @@ import geopandas as gpd
 import geopy
 from shapely.geometry import Point, Polygon
 
+from st_aggrid import AgGrid
 from geopy.geocoders import Nominatim
 from geopy.extra.rate_limiter import RateLimiter
 
@@ -34,10 +35,24 @@ def miles_to_meters(miles):
     except:
         return 0   
 
-
+@st.cache
+def convert_df(df):
+    # IMPORTANT: Cache the conversion to prevent computation on every rerun
+    return df.to_csv().encode('utf-8')
 
 @cache
-def get_map_data(coordinate):
+def get_map_data(coordinate): 
+
+    """
+    Returns a csv of schools and their information 
+    in a giving location.
+
+    params:
+        coordinate: Tuple
+    Returns:
+       a CSV of schools and their details from Google maps    
+    """
+
     map_client = googlemaps.Client(API_KEY)
 
     Location = coordinate
@@ -48,7 +63,7 @@ def get_map_data(coordinate):
 
     response = map_client.places_nearby(
         location=Location,
-        radius=10000,
+        radius=distance,
         type='school'
         )
     schools_list.extend(response.get('results'))
@@ -57,17 +72,44 @@ def get_map_data(coordinate):
     while next_page_token:
        time.sleep(5)
        response = map_client.places_nearby(
-        location=Location,
-        radius=1000,
-        page_token=next_page_token
-    )   
+           location=Location,
+           radius=1000,
+           page_token=next_page_token
+           )   
 
-    schools_list.extend(response.get('results'))
-    next_page_token = response.get('next_page_token')
+       schools_list.extend(response.get('results'))
+       next_page_token = response.get('next_page_token')
     df = pd.DataFrame(schools_list)
     df['url'] = 'https://www.google.com/maps/place/?q=place_id:' + df['place_id']
 
-    return st.dataframe(df)   
+    st.dataframe(df, 1000, 500)
+
+
+
+    client_ids = list(df['place_id'])
+
+    contact_list = []
+    for i in client_ids:
+        response = map_client.place(
+        place_id = i,
+        fields=['name', 'international_phone_number', 'formatted_address', 'website']
+        )
+
+        contact_list.append(response.get('result'))
+    
+    schDF = pd.DataFrame(contact_list)
+    st.dataframe(schDF)
+    
+    schDF = convert_df(schDF)
+
+    return st.download_button(label="Download data as CSV",
+                            data=schDF,
+                            file_name='large_df.csv',
+                            mime='text/csv',
+                        )
+                        
+
+
 
 
 Area = st.sidebar.text_input("Area", "lekki phase 1")
@@ -84,12 +126,12 @@ lon = location.longitude
 
 map_data = pd.DataFrame({'lat': [lat], 'lon': [lon]})
 
-st.map(map_data, zoom=30)
+st.map(map_data, zoom=11,)
 
 
 coordinate = (lat,lon)
-
 get_map_data(coordinate)
+
 
 
 
